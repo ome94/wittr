0645d5a4062bc150897301800dbd443b68ad78ff
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2024-08-03 15:41:13 +0100

    fix: Get photos after URL redirect

--- server/Server.js
+++ server/Server.js
@@ -101,7 +101,10 @@ export default class Server {
     this._app.get('/photos/:farm-:server-:id-:secret-:type.jpg', (req, res) => {
       const flickrUrl = `http://farm${req.params.farm}.staticflickr.com/${req.params.server}/${req.params.id}_${req.params.secret}_${imgSizeToFlickrSuffix[req.params.type]}.jpg`;
       const flickrRequest = http.request(flickrUrl, flickrRes => {
-        flickrRes.pipe(res);
+        Object.keys(flickrRes.headers).forEach((header) => {
+          res.setHeader(header, flickrRes.headers[header]);
+        });
+        res.sendStatus(flickrRes.statusCode);
       });
 
       flickrRequest.on('error', err => {
